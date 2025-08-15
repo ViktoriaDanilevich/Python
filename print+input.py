403eb283a267ec3061c74c90af945ccc0af5f11d
Viktory_QA <viktoria.danilevich99@gmail.com>
2025-08-15 10:01:36 +0300

    new_works

--- print+input.py
+++ print+input.py
@@ -578,3 +578,8 @@ if medium == max-min:
     print('Число интересное')
 else:
     print('Число неинтересное')
+
+#63. Даны пять чисел a1, a2, a3, a4, a5. Напишите программу, которая вычисляет сумму их модулей 
+a1, a2, a3, a4, a5 = float(input()), float(input()), float(input()), float(input()), float(input())
+result = abs(a1)+abs(a2)+abs(a3)+abs(a4)+abs(a5)
+print(result)
