46b4795de22fdeeedc1456f419f901587d60a057
Viktory_QA <viktoria.danilevich99@gmail.com>
2025-08-28 16:36:36 +0300

    new_practices

--- cycles.py
+++ cycles.py
@@ -27,3 +27,8 @@ for i in range(n+1):
 n = int(input())
 for i in range(n):
     print("*" * (n - i))
+
+#7. Даны два целых числа m и n (m≤n). Напишите программу, которая выводит все целые числа от m до n включительно.
+m, n = int(input()), int(input())
+for i in range(m, n+1):
+    print(i)
